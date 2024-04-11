1ca1a0f7c2cff4df7ddfdce0afbd103c5b41fb24
Gabriel Celestino <116518880+DevCelestino@users.noreply.github.com>
2024-04-11 01:40:49 -0300

    Delete front/src/pages/automacao/Etapas directory

--- front/src/pages/automacao/Etapas/index.tsx
+++
@@ -1,7 +0,0 @@
-export const Etapas = () => {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', height: '36.5px' }}>
-      <h1 style={{ fontSize: '1.5rem', fontWeight: 'bolder' }}>Automação / Etapas</h1>
-    </div>
-  );
-}
