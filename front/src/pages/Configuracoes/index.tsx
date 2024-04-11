fc3994716c993cdd3845dd175048f6e7d4bc5de1
Gabriel Celestino <116518880+DevCelestino@users.noreply.github.com>
2024-04-11 01:40:22 -0300

    Delete front/src/pages/Configuracoes directory

--- front/src/pages/Configuracoes/index.tsx
+++
@@ -1,7 +0,0 @@
-export const Configuracoes = () => {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', height: '36.5px' }}>
-      <h1 style={{ fontSize: '1.5rem', fontWeight: 'bolder' }}>Configurações</h1>
-    </div>
-  );
-}
