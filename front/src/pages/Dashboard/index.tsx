733d93de28dbb83ecc56d99f374fd354f5af3617
Gabriel Celestino <116518880+DevCelestino@users.noreply.github.com>
2024-04-11 01:40:13 -0300

    Delete front/src/pages/Dashboard directory

--- front/src/pages/Dashboard/index.tsx
+++
@@ -1,7 +0,0 @@
-export const Dashboard = () => {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', height: '36.5px' }}>
-      <h1 style={{ fontSize: '1.5rem', fontWeight: 'bolder' }}>Dashboard</h1>
-    </div>
-  );
-}
